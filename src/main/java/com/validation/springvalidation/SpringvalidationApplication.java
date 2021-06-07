555b93a95d8003a3b5d73e5ddff56b39e1eb1712
niksankarkee <karkeeniksan@gmail.com>
2021-06-07 17:15:49 +0900

    form validation Stater

--- src/main/java/com/validation/springvalidation/SpringvalidationApplication.java
+++ src/main/java/com/validation/springvalidation/SpringvalidationApplication.java
@@ -1,13 +1,13 @@
-package com.practice;
+package com.validation.springvalidation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThymeLeafPracticeApplication {
+public class SpringvalidationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ThymeLeafPracticeApplication.class, args);
+		SpringApplication.run(SpringvalidationApplication.class, args);
 	}
 
 }
--- src/test/java/com/validation/springvalidation/SpringvalidationApplicationTests.java
+++ src/test/java/com/validation/springvalidation/SpringvalidationApplicationTests.java
@@ -1,10 +1,10 @@
-package com.practice;
+package com.validation.springvalidation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PracticeApplicationTests {
+class SpringvalidationApplicationTests {
 
 	@Test
 	void contextLoads() {
