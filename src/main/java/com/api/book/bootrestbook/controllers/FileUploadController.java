baae53e1d038d637721f1d2e6aa26528358b7ae0
niksankarkee <karkeeniksan@gmail.com>
2021-06-07 11:16:45 +0900

    filehandling dymanic upload

--- src/main/java/com/api/book/bootrestbook/controllers/FileUploadController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/FileUploadController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class FileUploadController {
@@ -39,7 +40,8 @@ public class FileUploadController {
             boolean file = fileUploadHelper.uploadFile(image);
 
             if (file) {
-                return ResponseEntity.ok("File is successfully uploaded");
+                return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/image/")
+                        .path(image.getOriginalFilename()).toUriString());
             }
 
         } catch (Exception e) {
--- src/main/java/com/api/book/bootrestbook/helper/FileUploadHelper.java
+++ src/main/java/com/api/book/bootrestbook/helper/FileUploadHelper.java
@@ -1,35 +1,28 @@
 package com.api.book.bootrestbook.helper;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
 
-    public final String UPLOAD_DIR = "D:\\myWebsite\\Java\\springbootprojects\\bootrestbook\\src\\main\\resources\\static\\image";
+    public final String UPLOAD_DIR = new ClassPathResource("static/image").getFile().getAbsolutePath();
+
+    public FileUploadHelper() throws IOException {
+
+    }
 
     public boolean uploadFile(MultipartFile file) {
         boolean isFile = false;
 
         try {
-            // InputStream is = file.getInputStream();
-            // byte data[] = new byte[is.available()];
-
-            // is.read(data);
-
-            // // write
-            // FileOutputStream fos = new FileOutputStream(UPLOAD_DIR + File.separator +
-            // file.getOriginalFilename());
-
-            // fos.write(data);
-            // fos.flush();
-            // fos.close();
-            // isFile = true;
 
             Files.copy(file.getInputStream(), Paths.get(UPLOAD_DIR + File.separator + file.getOriginalFilename()),
                     StandardCopyOption.REPLACE_EXISTING);
